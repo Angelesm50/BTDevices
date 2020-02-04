6219d67771b292e9075d84bef34dfd3cf951d08b
Angeles Martinez <angelesmava0@gmail.com>
2020-02-03 22:57:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/trasem/DevicesBT.java
@@ -0,0 +1,82 @@
+package com.example.trasem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class DevicesBT extends AppCompatActivity {
+
+    ListView idList;
+
+    private static final String TAG = "DevicesBT";
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    private BluetoothAdapter mBtAdapter;
+    private ArrayAdapter<String> mPairedDevicesArrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices_bt);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        CheckStateBT();
+
+        mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+        idList = findViewById(R.id.IdListDevices);
+        idList.setAdapter(mPairedDevicesArrayAdapter);
+        idList.setOnItemClickListener(mDeviceClickListener);
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0){
+            for (BluetoothDevice device : pairedDevices){
+                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    }
+
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+
+            // Obtener la dirección MAC del dispositivo, que son los últimos 17 caracteres en la vista
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+            // Realiza un intent para iniciar la siguiente actividad
+            // mientras toma un EXTRA_DEVICE_ADDRESS que es la dirección MAC.
+            Intent i = new Intent(DevicesBT.this, MainActivity.class);//<-<- PARTE A MODIFICAR >->->
+            i.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            startActivity(i);
+        }
+    };
+
+    private void CheckStateBT() {
+        // Comprueba que el dispositivo tiene Bluetooth y que está encendido.
+        mBtAdapter= BluetoothAdapter.getDefaultAdapter();
+        if(mBtAdapter==null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+        } else {
+            if (mBtAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth Activado...");
+            } else {
+                //Solicita al usuario que active Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+
+            }
+        }
+    }
+}
